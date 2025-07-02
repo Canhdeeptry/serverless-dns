7e4b377d36ab3d1e8d9291856b4bf6f104b83ddd
Canhdeeptry <88322464+Canhdeeptry@users.noreply.github.com>
2025-07-02 19:02:50 +0700

    Update env.js

--- src/core/env.js
+++ src/core/env.js
@@ -117,7 +117,7 @@ const defaults = new Map(
     // primary doh upstream
     CF_DNS_RESOLVER_URL: {
       type: "string",
-      default: "https://cloudflare-dns.com/dns-query",
+      default: "https://1.1.1.1/dns-query",
     },
     // secondary doh upstream
     CF_DNS_RESOLVER_URL_2: {
